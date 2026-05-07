569ed07afb2fa422c6bc8d9876b2484b6b58de62
HebaElgohary <hebasamir535@gmail.com>
2026-05-07 23:12:08 +0300

    feat:add btn in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useRef } from "react";
 import { fade } from "@/src/animations/fade";
+import Button from "@/src/components/atoms/Button";
 export default function Home() {
   const boxRef = useRef(null);
   useEffect(() => {
@@ -12,6 +13,7 @@ export default function Home() {
     <div   ref={boxRef}
       className="!w-full min-h-screen p-10 bg-black text-white rounded-xl flex items-center justify-center" >
       home page
+      <Button className="ml-4 bg-green-300">button</Button>
     </div>
   );
 }
---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { cn } from '@/src/lib/cn'
+interface Props {
+    children: React.ReactNode
+    className?: string
+}
+export default function Button({children,className}: Props) {
+
+    return (
+    <button className={cn(`bg-red-300
+     hover:bg-red-400
+      text-white font-bold py-2 px-4 rounded ${className} `)}>
+        {children}</button>
+  )
+}
