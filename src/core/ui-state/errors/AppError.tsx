63ae8641068fe8ca88f3da43cf984a69bd078031
HebaElgohary <hebasamir535@gmail.com>
2026-05-08 22:52:18 +0300

    feat: add errors folder

---
+++ src/core/ui-state/errors/AppError.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AppError() {
+  return (
+    <div>AppError</div>
+  )
+}
---
+++ src/core/ui-state/errors/validationError.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function validationError() {
+  return (
+    <div>validationError</div>
+  )
+}
