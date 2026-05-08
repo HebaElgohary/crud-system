8ff666bfbe783176fe4b8393759a78f09fd88af0
HebaElgohary <hebasamir535@gmail.com>
2026-05-08 22:41:12 +0300

    fix axios config

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,4 +1,9 @@
+
 export const apiClient={
+    get(url:string){
+        return axios.get(url)
+    }
+    
     
     
 }
--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -1,6 +1,9 @@
 import axios,{AxiosInstance} from "axios";
 import {env} from "@/src/config/ENV"
+import { tokenServices } from "./tokenService";
 
+
+//axios instance configuration
 const axiosInstance:AxiosInstance=axios.create({
     baseURL:env.BASE_URL,
     timeout:env.API_TIMEOUT,
@@ -8,3 +11,16 @@ const axiosInstance:AxiosInstance=axios.create({
     headers:{"Content-Type":"application/json"
         ,"Accept":"application/json"},
 })
+////////////////////////////////////
+
+
+// request interceptor
+axiosInstance.interceptors.request.use((config) => {
+    const token = tokenServices.getToken();
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  }, (error) => {
+    return Promise.reject(error);
+  });
