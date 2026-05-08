db3266824d5aad66533041a069a281cb986a50c1
HebaElgohary <hebasamir535@gmail.com>
2026-05-08 18:08:30 +0300

    add config folder && env file

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { cn } from '@/src/lib/cn'
+import React from "react";
+import { cn } from "@/src/lib/cn";
 interface Props {
-    children: React.ReactNode
-    className?: string
+  children: React.ReactNode;
+  className?: string;
 }
-export default function Button({children,className}: Props) {
-
-    return (
-    <button className={cn(`bg-red-300
+export default function Button({ children, className }: Props) {
+  return (
+    <button
+      className={cn(`bg-red-300
      hover:bg-red-400
-      text-white font-bold py-2 px-4 rounded ${className} `)}>
-        {children}</button>
-  )
+      text-white font-bold !py-2 px-4 rounded ${className} `)}
+    >
+      {children}
+    </button>
+  );
 }
---
+++ src/config/ENV.ts
@@ -0,0 +1,8 @@
+export const env={
+    DEV_MODE:"development",
+    PROD_MODE:"production",
+    BASE_URL:"http://localhost:3000",
+    ACCES_TOKEN_KEY:"access_token",
+    REFRESH_TOKEN_KEY:"refresh_token",
+
+}
