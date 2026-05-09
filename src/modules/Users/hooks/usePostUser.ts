9b185aedfa406c0a76608e51236d7f554c1e5f11
HebaElgohary <hebasamir535@gmail.com>
2026-05-09 17:49:31 +0300

    feat: add usePostUser

---
+++ src/modules/Users/hooks/usePostUser.ts
@@ -0,0 +1,17 @@
+import { useApiMutation } from "@/src/hooks/useApiMutation";
+import { postUserApi } from "../api/postUserApi";
+import { useQueryClient } from "@tanstack/react-query";
+import{tokenServices} from "../../../services/tokenService"
+export const usePostUser = () => {
+  const queryClient = useQueryClient();
+  return useApiMutation({
+    mutationFn: postUserApi,
+    options: {
+
+      onSuccess: (data) => {
+        queryClient.invalidateQueries({ queryKey: ["users"] });
+        tokenServices.setToken(data.accessToken);
+      },
+    },
+  });
+};
