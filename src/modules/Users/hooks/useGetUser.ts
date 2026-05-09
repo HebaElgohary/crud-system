fafbd267f67cca4ad41452f81d658e719ce796b6
HebaElgohary <hebasamir535@gmail.com>
2026-05-09 17:24:07 +0300

    feat: add useGetUser

---
+++ src/modules/Users/hooks/useGetUser.ts
@@ -0,0 +1,10 @@
+import { useApiQuery } from "@/src/hooks/useApiQuery";
+import {getUserApi} from "../api/getUserApi";
+
+export const useGetUser=(id:number)=>{
+    return useApiQuery({
+        queryKey:['user'],
+        queryFn:()=>getUserApi(id)
+    })
+
+}
