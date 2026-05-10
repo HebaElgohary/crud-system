7e4f4aaaf50b220d441f67061a741f74adb4582f
HebaElgohary <hebasamir535@gmail.com>
2026-05-10 19:10:53 +0300

    feat: add bg utilities in globals file

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -3,11 +3,22 @@ import { cn } from "@/src/lib/cn";
 interface Props {
   children: React.ReactNode;
   className?: string;
+  variant?: 'green'|'blue'|'violet'|'warning'|'danger';
 }
 export default function Button({ children, className }: Props) {
+ 
+ const variants = {
+   green: 'bg-green-500',
+   blue: 'bg-blue-500',
+   violet: 'bg-violet-500',
+   warning: 'bg-warning-500',
+   danger: 'bg-danger-500',
+ }
+
+ 
   return (
     <button
-      className={cn(`bg-red-300
+      className={cn(`${}
      hover:bg-red-400
       text-white font-bold !py-2 px-4 rounded ${className} `)}
     >
