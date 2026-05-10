9ef519c2d8458be10ac647c50050c6872c94d0f9
HebaElgohary <hebasamir535@gmail.com>
2026-05-10 19:13:58 +0300

    feat: add variant prop to button component

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -5,22 +5,22 @@ interface Props {
   className?: string;
   variant?: 'green'|'blue'|'violet'|'warning'|'danger';
 }
-export default function Button({ children, className }: Props) {
+export default function Button({ children, className, variant='green' }: Props) {
  
  const variants = {
-   green: 'bg-green-500',
-   blue: 'bg-blue-500',
-   violet: 'bg-violet-500',
-   warning: 'bg-warning-500',
-   danger: 'bg-danger-500',
+   green: '.ds-bg-green',
+   blue: '.ds-bg-blue',
+   violet: '.ds-bg-violet',
+   warning: '.ds-bg-warning',
+   danger: '.ds-bg-danger',
  }
 
  
   return (
     <button
-      className={cn(`${}
+      className={cn(`${variants[variant ]}
      hover:bg-red-400
-      text-white font-bold !py-2 px-4 rounded ${className} `)}
+      text-white font-bold !py-2 !px-4 rounded ${className} `)}
     >
       {children}
     </button>
