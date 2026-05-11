b20342a3df09d2d1018bb85afed301de82fafd05
HebaElgohary <hebasamir535@gmail.com>
2026-05-11 22:37:01 +0300

    fix input & password styles

--- src/components/atoms/Input.tsx
+++ src/components/atoms/Input.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
 
-export default function Input({name,id,type,placeholder}:{name:string,id:string,type:string,    placeholder:string}) {
+export default function Input({
+  name,
+  id,
+  type,
+  placeholder,
+}: {
+  name: string;
+  id: string;
+  type: string;
+  placeholder: string;
+}) {
   return (
-    <input id={id} name={name} type={type} placeholder={placeholder} className='border border-gray-400 !px-4 !py-2 rounded-md'  />
-  )
+    <input
+      id={id}
+      name={name}
+      type={type}
+      placeholder={placeholder}
+      className="border border-gray-400 !px-4 !py-2 rounded-md   focus:outline-none
+    focus:ring-2
+    focus:ring-violet-400
+    focus:border-violet-500"
+    />
+  );
 }
--- src/components/atoms/password.tsx
+++ src/components/atoms/password.tsx
@@ -1,14 +1,50 @@
 import React, { useState } from 'react'
-import {Eye, EyeOff} from '../../assets/icons/icons'
+import { Eye, EyeOff } from '../../assets/icons/icons'
 import Icon from './Icon'
-export default function Password({name,id,type,placeholder}:{name:string,id:string,type:string,    placeholder:string}) {
-   const [showPassword, setShowPassword] = useState(false);
-    return (
-    <div className='border border-gray-400 !px-4 !py-2 rounded-md flex justify-between'>
-    <input id={id} name={name} type={showPassword ? 'text' : type} placeholder={placeholder}  />
-    {/* <button type='button' onClick={() => setShowPassword(!showPassword)} className='p-0 bg-transparent border-0'> */}
-      <Icon Icon={showPassword ? EyeOff : Eye} className='ds-text-violet' onClick={()=>setShowPassword(!showPassword)}/>
-    {/* </button> */}
-  </div>
+
+export default function Password({
+  name,
+  id,
+  type,
+  placeholder
+}: {
+  name: string
+  id: string
+  type: string
+  placeholder: string
+}) {
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  return (
+
+    <div
+      className='
+        border border-gray-400
+        !px-4 !py-2
+        rounded-md
+        flex justify-between items-center
+
+        focus-within:ring-2
+        focus-within:ring-violet-400
+        focus-within:border-violet-500
+      '
+    >
+
+      <input
+        className='w-full focus:outline-none'
+        id={id}
+        name={name}
+        type={showPassword ? 'text' : type}
+        placeholder={placeholder}
+      />
+
+      <Icon
+        Icon={showPassword ? EyeOff : Eye}
+        className='ds-text-violet cursor-pointer'
+        onClick={() => setShowPassword(!showPassword)}
+      />
+
+    </div>
   )
 }
