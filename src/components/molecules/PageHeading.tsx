36016a50594878a87bef3be02300767ca7add121
HebaElgohary <hebasamir535@gmail.com>
2026-05-12 03:49:42 +0300

    feat: renderd themeToggleBtn in PageHeading

--- src/components/molecules/PageHeading.tsx
+++ src/components/molecules/PageHeading.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Title from '../atoms/Title'
 import Subtitle from '../atoms/Subtitle'
 import {PageHeadingProps} from '../../types/types'
+import ThemeToggleButton from './ThemeToggleButton'
 
 
 export default function PageHeading({title,subtitle}:PageHeadingProps) {
   return (
-    <div className='flex flex-col gap-2  border-b border-gray-200 !px-2 !py-4'>
+    <div className='flex flex-row gap-2  border-b border-gray-200 !px-2 !py-4'>
+    <div className='flex flex-col gap-2  '>
         <Title>{title}</Title>
         <Subtitle>{subtitle}</Subtitle>
+    </div>
+    <ThemeToggleButton />
     </div>
   )
 }
