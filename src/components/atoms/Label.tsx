49e40693a4fb44e8cffa79d45d23eb5d02b9b6bd
HebaElgohary <hebasamir535@gmail.com>
2026-05-12 15:54:04 +0300

    fix: label dark theme color

--- src/components/atoms/Label.tsx
+++ src/components/atoms/Label.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Label({label,name}:{label:string,name:string}) {
   return (
-    <label className='font-bold text-lg' htmlFor={name}>{label}</label>
+    <label className='font-bold text-lg ds-text-black' htmlFor={name}>{label}</label>
   )
 }
