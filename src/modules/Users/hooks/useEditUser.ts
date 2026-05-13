7d64c0126652e3d07393833488c5d77c20d31e33
HebaElgohary <hebasamir535@gmail.com>
2026-05-13 14:47:21 +0300

    feat: install sweetalert2

--- src/modules/Users/hooks/useEditUser.ts
+++ src/modules/Users/hooks/useEditUser.ts
@@ -2,6 +2,7 @@ import { useApiMutation } from "@/src/hooks/useApiMutation";
 import {editUserApi} from "../api/editUserApi"
 import { useQueryClient } from "@tanstack/react-query";
 import { editPayload } from "../types/types";
+import Swal from "sweetalert2";
 export const useEditUser = (id:number) => {
     const queryClient = useQueryClient();
     return useApiMutation({
@@ -9,7 +10,20 @@ export const useEditUser = (id:number) => {
         options: {
             onSuccess: (data) => {
                 queryClient.invalidateQueries({ queryKey: ["user",id] });
+                   Swal.fire({
+      title: "Edited!",
+      text: "User Updated successfully",
+      icon: "success",
+    });
             },
+
+  onError: () => {
+    Swal.fire({
+      title: "Error!",
+      text: "Failed to Update user",
+      icon: "error",
+    });
+  },
         },
     });
 };
--- src/modules/Users/hooks/usePostUser.ts
+++ src/modules/Users/hooks/usePostUser.ts
@@ -2,15 +2,29 @@ import { useApiMutation } from "@/src/hooks/useApiMutation";
 import { postUserApi } from "../api/postUserApi";
 import { useQueryClient } from "@tanstack/react-query";
 import{tokenServices} from "../../../services/tokenService"
+import Swal from "sweetalert2";
 export const usePostUser = () => {
   const queryClient = useQueryClient();
   return useApiMutation({
     mutationFn: postUserApi,
     options: {
 
+
       onSuccess: (data) => {
         queryClient.invalidateQueries({ queryKey: ["users"] });
+            Swal.fire({
+      title: "Added!",
+      text: "User added successfully",
+      icon: "success",
+    });
       },
+      onError: (error) => {
+            Swal.fire({
+      title: "Error!",
+      text: "Failed to Add user",
+      icon: "error",
+    });
+      }
     },
   });
 };
