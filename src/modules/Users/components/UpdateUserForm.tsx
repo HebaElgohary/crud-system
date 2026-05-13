436433cf6bafcb6c7b66e5eab88c1dbfab84e59e
HebaElgohary <hebasamir535@gmail.com>
2026-05-13 22:48:23 +0300

    feat: add fade animation to updateForm

--- src/modules/Users/components/UpdateUserForm.tsx
+++ src/modules/Users/components/UpdateUserForm.tsx
@@ -3,10 +3,19 @@ import UpdateForm from '@/src/components/molecules/form/UpdateForm'
 import {useEditUser} from '@/src/modules/Users/hooks/useEditUser'
 import {useGetUser} from '@/src/modules/Users/hooks/useGetUser'
 import { editPayload } from '../types/types'
+import { useEffect, useRef } from 'react'
+import { fade } from '@/src/animations/fade'
 export default function UpdateUserForm({id}:{id:number}) {
     const {mutate}=useEditUser(id)
     const {data}=useGetUser(id)
     console.log('dataaa user is ',data)
+          const cardRef = useRef<HTMLDivElement>(null);
+      useEffect(() => {
+        if (cardRef.current) {
+          fade(cardRef.current);
+        }
+      }, [data])
+        
 
     const onsubmit=(data:editPayload)=>
         
@@ -14,7 +23,7 @@ export default function UpdateUserForm({id}:{id:number}) {
          }
   return (
     <div>
-      {id&& <UpdateForm userData={data} userId={id} onSubmit={onsubmit}/>}
+      {id&& <UpdateForm ref={cardRef} userData={data} userId={id} onSubmit={onsubmit}/>}
     </div>
   )
 }
