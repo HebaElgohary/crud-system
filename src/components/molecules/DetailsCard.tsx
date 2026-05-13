4bd49a7c268a04a179741bf1ad414a09ffbb37d1
HebaElgohary <hebasamir535@gmail.com>
2026-05-13 21:52:22 +0300

    fix

--- src/components/molecules/DetailsCard.tsx
+++ src/components/molecules/DetailsCard.tsx
@@ -8,7 +8,7 @@ export default function DetailsCard({data}:{data:user}) {
     <div className='flex ds-bg-main !p-5 rounded-xl h-fit flex-col border border-gray-300 '>
         <FormHeading label='User Details' icon={User} iconColor='blue' titleVariant='black'/>
    {/* id */}
-    <div className='flex flex-row gap-11 items-center ds-text-black !mb-6'>
+    <div className='flex flex-row gap-20 items-center ds-text-black !mb-6'>
 <span>ID</span>
 <span>{data.id}</span>
     </div>
