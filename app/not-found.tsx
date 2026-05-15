e3ca8c40b4ace3eb844a9fdf678a2d9327cd25ed
HebaElgohary <hebasamir535@gmail.com>
2026-05-15 03:15:27 +0300

    feat: add not found ui-state

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,25 +1,8 @@
-'use client'
 
-import Button from '@/src/components/atoms/Button'
-import Title from '@/src/components/atoms/Title'
-import dynamic from 'next/dynamic'
-
-const Lottie = dynamic(
-  () => import('@lottiefiles/react-lottie-player').then(mod => mod.Player),
-  { ssr: false }
-)
+import NotFoundState from '@/src/core/ui-state/NotFoundState'
 
 export default function Notfound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Lottie
-        autoplay
-        loop
-        src="/lottie/notFound.json"
-        style={{ height: '300px', width: '300px' }}
-      />
-      <Title  >404 Not Found</Title>
-      <Button className="btn btn-primary" variant="danger" size="sm" onClick={() => window.location.reload()}>Reload</Button>
-    </div>
+   <NotFoundState />
   )
 }
--- src/core/ui-state/NotFoundState.tsx
+++ src/core/ui-state/NotFoundState.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import Button from '@/src/components/atoms/Button'
+import Title from '@/src/components/atoms/Title'
+import dynamic from 'next/dynamic'
 
+const Lottie = dynamic(
+  () => import('@lottiefiles/react-lottie-player').then(mod => mod.Player),
+  { ssr: true }
+)
 export default function NotFoundState() {
   return (
-    <div>NotFoundState</div>
-  )
+ <div className="flex flex-col items-center justify-center h-screen">
+      <Lottie
+        autoplay
+        loop
+        src="/lottie/notFound.json"
+        style={{ height: '300px', width: '300px' }}
+      />
+      <Title  >404 Not Found</Title>
+      <Button className="btn btn-primary" variant="danger" size="sm" onClick={() => window.location.reload()}>Reload</Button>
+    </div>  )
 }
