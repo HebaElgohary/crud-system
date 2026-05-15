4b20fbb9e9171b965b94e481869a9e6bd39ed60f
HebaElgohary <hebasamir535@gmail.com>
2026-05-15 03:05:05 +0300

    feat: add loading page

--- app/Loading.tsx
+++ app/Loading.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+'use client'
+
+import dynamic from 'next/dynamic'
+
+const Lottie = dynamic(
+  () => import('@lottiefiles/react-lottie-player').then(mod => mod.Player),
+  { ssr: false }
+)
 
 export default function Loading() {
   return (
-    <div>Loading</div>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Lottie
+        autoplay
+        loop
+        src="/lottie/Loading.json"
+        style={{ height: '300px', width: '300px' }}
+      />
+
+    </div>
   )
 }
